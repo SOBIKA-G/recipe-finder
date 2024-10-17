739da5949b44f7a913e9ac53a86f68ea2c939f9b
SOBIKA-G <sobika751@gmail.com>
2024-10-17 09:47:58 +0530

    Initial commit of RecipeFinder project with Jenkins setup

--- recipe_env/Scripts/activate.bat
+++ recipe_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sobik\OneDrive\Documents\DevOps\Recipe FInder\recipe_env
+set VIRTUAL_ENV=C:\Users\sobik\OneDrive\Documents\DevOps\RecipeFinder\recipe_env
 
 if not defined PROMPT set PROMPT=$P$G
 
