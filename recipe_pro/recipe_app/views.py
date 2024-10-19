bcb301af64fe8f80725c586becd8035d037d79e8
SOBIKA-G <sobika751@gmail.com>
2024-10-19 21:36:21 +0530

    Modifying views

--- recipe_pro/recipe_app/views.py
+++ recipe_pro/recipe_app/views.py
@@ -6,14 +6,14 @@ from django.template import loader
 def index(request):
     res = Recipe.objects.all()
     sample_recipes = Recipe.objects.all()[:5]
-    return render(request, 'home.html', {
+    return render(request, 'templates/home.html', {
         'res': res,
         'sample_recipes': sample_recipes})
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, 'recipe_details.html', {'recipe': recipe})
+    return render(request, 'templates/recipe_details.html', {'recipe': recipe})
 
 def search_recipes(request):
     query = request.GET.get('search')
     recipes = Recipe.objects.filter(title__icontains=query)
-    return render(request, 'search_results.html', {'recipes': recipes, 'query': query})
+    return render(request, 'templates/search_results.html', {'recipes': recipes, 'query': query})
